fedf5cc50c1b55d61749e3a3329d88ffce46e7bf
josafatprog <josafatgarol.garol@gmail.com>
2026-05-10 17:02:40 -0600

    Se agrega main en ruta de arranque

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port = 5000)
+
 @app.route('/calcular', methods=['POST'])
 def calcular():
     try:
